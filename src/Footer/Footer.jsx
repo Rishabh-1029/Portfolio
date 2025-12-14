22953118c60836ecc1cd53b35cc633adf566ff0e
Rishabh Surana <rishabhjainsurana@gmail.com>
2025-12-14 23:31:48 +0530

    Footer Section

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer class="footer">
+      <p>
+        <span class="footer-quote">Creating, Learning, and Evolving</span>
+        <span class="footer-divider">|</span>© 2025{" "}
+        <strong>Rishabh Surana</strong>. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
